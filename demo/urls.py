e8a26f8d3413fc31f0628f65fced4b1d49ba2e3d
Hack4u2 <willmer0204@gmail.com>
2025-07-18 02:46:51 -0400

    Fix Health_Check Problem

--- demo/urls.py
+++ demo/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, healthcheck  # importa tu healthcheck también
+from api.views import UserViewSet, health_check
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('health/', healthcheck),
+    path('health/', health_check),
     path('', include(router.urls)),
 ]
