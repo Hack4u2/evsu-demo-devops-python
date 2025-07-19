ab67369eb3d661483dcae8dfaf38578d49b5e32a
Hack4u2 <willmer0204@gmail.com>
2025-07-19 15:00:35 -0400

    Try,v4

--- demo/settings.py
+++ demo/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 # Variables desde .env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["35.224.122.238"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["0.0.0.0", "localhost", "35.224.122.238"])
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
