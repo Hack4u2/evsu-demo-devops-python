29328669afa0ed240bd3dc5c766bd180864c302b
Hack4u2 <willmer0204@gmail.com>
2025-07-19 22:01:51 -0400

    fix: Fixed flake8 E261 style error in settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/staticfiles/' # Ruta absoluta para los archivos estáticos recolectados
+STATIC_ROOT = '/app/staticfiles/'
 
 # Configuración de Whitenoise para servir archivos estáticos comprimidos y con caché
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
